4f9b71507234ab84b1b00be5a8d21e147fc2fdea
Rizner17 <ajrod9909@gmail.com.noreply.github.com>
2021-04-08 18:24:10 +0000

    python and excel, including working with pandas

--- mygraph.py
+++ mygraph.py
@@ -4,8 +4,6 @@
 # python3 -m pip install np
 import numpy as np
 # python3 -m pip install matplotlib
-import matplotlib
-matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
 
 def main():
@@ -15,12 +13,12 @@ def main():
 
     fig, ax = plt.subplots()
 
-    ax.bar(labels, week_temps, width, label='Week')
+    ax.bar(labels, week_temps, width, label='Week') # shows the labels along the x-axis, takes the width from         above setting the bars to .25 width, dsisplays the week temperatures on the y-axis, and shows the label Week      above the bars.
 
-    ax.set_ylabel('Temperature(Degrees)')
-    ax.set_title('Orlando Weekly Forecast')
+    ax.set_ylabel('Temperature(Degrees)')#sets the y-axis as Temperature in degrees
+    ax.set_title('Orlando Weekly Forecast')#sets the title as Orlando weekly forecast
     ax.legend()
-    # plt.show() # you can try this on a Python IDE with a GUI if you'd like
+    # plt.savefigure will save it in this directory as a pdf
     plt.savefig("/home/student/mycode/graphing/Orlandoforcast.pdf")
     # save a copy to "~/static" (the "files" view)
     plt.savefig("/home/student/static/Orlandoforcast.pdf")
